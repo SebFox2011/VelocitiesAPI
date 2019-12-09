d21f52c07d4ac8dc4aaa8a081a30f3a4540073eb
Seb <sebastien.philippe5@gmail.com>
2019-12-09 10:20:53 +0100

    Ajout de la route stationsRouter

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require ('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var stationsRouter = require('./routes/stations');
 
 var app = express();
 
@@ -16,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/stations', stationsRouter);
 
 module.exports = app;
