d1f89a9df715d7ef1362cc210ce0907b92f56b6b
Seb <sebastien.philippe5@gmail.com>
2019-12-09 16:21:33 +0100

    Ajout de l'url API de Paris

--- routes/stations.js
+++ routes/stations.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const router = express.Router();
 ObjectID = require('mongodb').ObjectID;
+const axios = require ('axios');
+
+const API_END_POINT_Rennes = "https://data.rennesmetropole.fr/api/records/1.0/search/?";
+const STATIONS_Rennes = "dataset=etat-des-stations-le-velo-star-en-temps-reel&facet=nom" +
+    "&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles" +
+    "&facet=nombrevelosdisponibles&rows=54";
+
+const API_END_POINT_PARIS = "https://opendata.paris.fr/api/records/1.0/search/?"
+
+const STATION_Paris="dataset=velib-disponibilite-en-temps-reel&facet=creditcard&facet=station_state&facet=station_name&rows=1000"
+
+function getStationRennes() {
+    axios.get(`${API_END_POINT_Rennes}${STATIONS_Rennes}`).then((response) => {
+        console.log(response.data.records);
+        getStationParis();
+
+    });
+}
+
+function getStationParis () {
+    axios.get(`${API_END_POINT_PARIS}${STATION_Paris}`).then((response) => {
+        console.log(response.data.records);
+    });
+}
 
 router.get('/',function (req,res,next){
 
@@ -14,7 +38,8 @@ router.get('/',function (req,res,next){
             name:"Gare"
         }
     ];
-
+    getStationRennes();
+    //getStationParis();
     /*res.status(200).send('Stations de vélo');
     next();
     res.json(stuff);*/
