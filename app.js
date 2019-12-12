11518321a2584c80df1317fd62113414251d1680
Seb <sebastien.philippe5@gmail.com>
2019-12-12 10:12:44 +0100

    Ajout de la politique CORS à l'API

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
